4d6f00c7a378db7b2e5d876f7a759f0eb57768ac
Han Lee <hi385790@gmail.com>
2025-04-21 01:05:39 +0900

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase SDK v9 모듈 방식 (ES Module)
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
